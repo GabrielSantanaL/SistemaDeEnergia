065399b06cb2916f229e7e42201579e63b40e7ae
aluno <aluno@172.16.3.22>
2022-12-20 16:56:11 -0300

    Pessoa Fisica

--- SistemaDeEnergia/src/sistemaenergia/PessoaFisica.java
+++ SistemaDeEnergia/src/sistemaenergia/PessoaFisica.java
@@ -2,24 +2,28 @@ package sistemaenergia;
 
 public class PessoaFisica  extends Cliente{
 	
-	private String CPF;
+	  private String CPF;
 
-	public String getCPF() {
-		return CPF;
-	}
+	    public String getCPF() {
+	        return CPF;
+	    }
 
-	public void setCPF(String cPF) {
-		CPF = cPF;
+	    public void setCPF(String cPF) {
+	        CPF = cPF;
+	    }
+	    
+	   public String toString() {
+	    String texto = "";
+	        
+	        texto = "----------------------------"
+	                +"\n Rua: " + getRua()
+	                +"\n Rua: " + getNumero()
+	                +"\n CEP: " + getCep()
+	                +"\n Cidade: " + getCidade()
+	                +"\n Estado: " + getEstado();
+	        
+	        return texto;
+	                
+	    
+	    }
 	}
-	
-   public String toString() {
-	String texto = "";
-		
-		texto = "----------------------------"
-				+"\n CPF: " + getCPF();
-				
-		return texto;
-				
-	
-	}
-}
